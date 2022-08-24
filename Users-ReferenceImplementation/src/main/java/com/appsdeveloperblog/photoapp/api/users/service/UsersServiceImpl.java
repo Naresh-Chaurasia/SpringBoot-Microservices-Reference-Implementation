f6857f388cf899b35192d94e948734fb02dae714
Naresh Chaurasia <nareshchaurasia@Nareshs-MacBook-Pro.local>
2022-08-24 19:28:21 +0530

    Macbook Pro, Checkin Timestamp::Wed Aug 24 19:28:21 IST 2022

---
+++ Users-ReferenceImplementation/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UsersServiceImpl.java
@@ -0,0 +1,43 @@
+package com.appsdeveloperblog.photoapp.api.users.service;
+
+import com.appsdeveloperblog.photoapp.api.users.data.UserEntity;
+import com.appsdeveloperblog.photoapp.api.users.data.UsersRepository;
+import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+
+	UsersRepository usersRepository;
+
+
+	@Autowired
+	public UsersServiceImpl(UsersRepository usersRepository)
+	{
+		this.usersRepository = usersRepository;
+	}
+	
+
+	@Override
+	public UserDto createUser(UserDto userDetails) {
+		userDetails.setUserId(UUID.randomUUID().toString());
+
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
+		userEntity.setEncryptedPassword("test");
+		usersRepository.save(userEntity);
+
+		return null;
+	}
+
+}
--- Users-ReferenceImplementation/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ Users-ReferenceImplementation/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -1,6 +1,10 @@
 package com.appsdeveloperblog.photoapp.api.users.ui.controllers;
 
+import com.appsdeveloperblog.photoapp.api.users.service.UsersService;
+import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
 import com.appsdeveloperblog.photoapp.api.users.ui.model.CreateUserRequestModel;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +19,9 @@ public class UsersController {
 	@Autowired
 	private Environment env;
 
+	@Autowired
+	UsersService usersService;
+
 
 	@GetMapping("/status/check")
 	public String status()
@@ -25,6 +32,13 @@ public class UsersController {
 	@PostMapping
 	public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails)
 	{
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+		UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+
+		UserDto createdUser = usersService.createUser(userDto);
+
 		return "Create User Post";
 	}
 }
