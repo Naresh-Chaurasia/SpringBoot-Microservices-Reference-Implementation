db2b5830a7ce0cbbf8f7a1d46380bde2566eeb51
Naresh Chaurasia <nareshchaurasia@Nareshs-MacBook-Pro.local>
2022-09-27 12:14:34 +0530

    Macbook Pro, Checkin Timestamp::Tue Sep 27 12:14:34 IST 2022

--- Users-RI/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ Users-RI/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -1,6 +1,9 @@
 package com.appsdeveloperblog.photoapp.api.users.ui.controllers;
 
+import com.appsdeveloperblog.photoapp.api.users.service.UsersService;
 import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
+import com.appsdeveloperblog.photoapp.api.users.ui.model.CreateUserRequestModel;
+import com.appsdeveloperblog.photoapp.api.users.ui.model.CreateUserResponseModel;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +23,33 @@ public class UsersController {
 	@Autowired
 	private Environment env;
 
+	@Autowired
+	UsersService usersService;
+
 
 	@GetMapping("/status/check")
 	public String status() {
 		return "Working on port " + env.getProperty("local.server.port");
+	}
+
+	@PostMapping(
+			consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE },
+			produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }
+	)
+	public ResponseEntity<CreateUserResponseModel>  createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+
+		System.out.println("-----------------------------UsersController/createUser--------------------------");
+
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+		UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+
+		UserDto createdUser = usersService.createUser(userDto);
+
+		CreateUserResponseModel returnValue = modelMapper.map(createdUser, CreateUserResponseModel.class);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
 	}
 
 }
